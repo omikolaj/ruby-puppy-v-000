3d1970479602014375afe669e843c3b70e2b9f65
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-11 10:00:31 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+attr_accessor name:
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |name|
+      puts "#{name}"
+    
+  end
+
+end
